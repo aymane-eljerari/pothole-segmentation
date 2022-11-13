3e7045c022d93568ba8830721b3952661d507df2
Aymane <aymaneljerari@gmail.com>
2022-11-13 10:54:09 -0500

    add pothole600 structure

---
+++ src/data/segmentation.py
@@ -0,0 +1,41 @@
+import os
+import shutil
+
+
+'''
+    (String)        path: local data directort
+'''
+def concatenate_data(path):
+    data_path = "data/segmentation/raw"
+    folders = ['training', 'testing', 'validation']
+    subfolders = ['rgb/', 'label/']
+    count = 0
+
+    # Check if directory exists
+    if not os.path.exists(data_path):
+        os.mkdir(data_path)
+
+    # Create sub directories
+    if not next(os.scandir(data_path), None):
+        for subfolder in subfolders:
+            os.makedirs(os.path.join(data_path, subfolder))
+        print("Created segmentation data directorty.")
+    
+    # Copy all files into input and labels
+    for subfolder in subfolders:
+        for folder in folders:
+            for file_name in os.listdir(os.path.join(path, folder, subfolder)):
+                count += 1
+                source = os.path.join(path, folder, subfolder, file_name)
+                destination = os.path.join(data_path, subfolder)
+                if os.path.isfile(source):
+                    shutil.copy(source, destination)
+                    old_name = os.path.join(destination, file_name)
+                    new_name = os.path.join(destination, str(count)+'.png')
+                    os.rename(old_name, new_name)
+    
+    print("Files organized successfully.")
+
+if __name__ == '__main__':
+    path = "/Users/aymane/Downloads/pothole600/"
+    concatenate_data(path)
