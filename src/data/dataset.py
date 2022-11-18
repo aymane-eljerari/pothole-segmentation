6b7a0a897ef3931210ce8f88fe9f8fb627efaf77
Aymane <aymaneljerari@gmail.com>
2022-11-18 17:02:23 -0500

    add data augmentation

--- src/data/dataset.py
+++ src/data/dataset.py
@@ -1,61 +1,98 @@
 import os
-import cv2
-import numpy as np
-import pandas as pd
+import random
+
 import torch
 from PIL import Image
 from torch.utils.data import Dataset
 from torchvision import transforms
-from transformers import SegformerFeatureExtractor
+import torchvision.transforms.functional as TF
+
 
 # Link to dataset: https://sites.google.com/view/pothole-600/dataset
 class SegDataset(Dataset):
     '''Pothole Segmentation Dataset dataset.'''
 
-    def __init__(self, path, feature_extractor):
+    def __init__(self, path):
         '''
         Args:
-            
-            parh                (string): directory containing all images
-            feature_exctractor  (SegformerFeatureExtractor): Segformer input encoder from Hugging Face
+            path                (string):                       directory containing all images
+            feature_exctractor  (SegformerFeatureExtractor):    Segformer input encoder from Hugging Face
         '''
         self.path               = path
+
         self.data_path          = os.path.join(self.path, 'rgb')
         self.label_path         = os.path.join(self.path, 'label')
+
         self.data               = torch.zeros((600, 3, 400, 400))
-        self.labels             = torch.zeros((600, 1, 400, 400))
-        self.feature_extractor  = feature_extractor
+        self.label              = torch.zeros((600, 1, 400, 400))
+
+        self.data_augment       = torch.zeros((7800, 13, 3, 400, 400))
+        self.label_augment      = torch.zeros((7800, 13, 1, 400, 400))
+
+        self.angles             = [-90, -60, -45, -30, -15, 15, 30, 45, 60, 90]
 
         self.generate_data()
-
-    
-    def generate_data(self):
-        '''
-        Generates and stores the data and labels
-        '''
-        for i in range(600):
-            img             = cv2.imread(os.path.join(self.data_path, str(i+1) + '.png'))
-
-            segment_image   = cv2.imread(os.path.join(self.label_path, str(i+601) + '.png'))
-            segment_image   = cv2.cvtColor(segment_image, cv2.COLOR_RGB2GRAY)
-
-            encoded_input   = self.feature_extractor(img, segment_image, return_tensors="pt")
-
-            self.data[i]    = encoded_input['pixel_values'].squeeze()
-            self.labels[i]  = encoded_input['labels'].squeeze()
 
     def __len__(self):
         return len(self.data)
 
     def __getitem__(self, idx):
-        return self.data[idx]
+        return self.data_augment[idx], self.label_augment[idx]
 
+    
+    def generate_data(self):
+        #TODO: dont forget random seed initialization for data transforms
+        '''
+        Generates and stores the data and labels
+        '''
+        # Generate Initial Dataset
+        for i in range(600):
+            img             = Image.open(os.path.join(self.data_path, str(i+1) + '.png'))
+            label           = Image.open(os.path.join(self.label_path, str(i+601) + '.png')).convert('L')
+
+            transform       = transforms.Compose([transforms.PILToTensor()])
+            img_tensor      = torch.tensor(transform(img)).detach().clone().squeeze_()
+            label_tensor    = torch.tensor(transform(label)).detach().clone().squeeze_()
+
+            self.data[i]    = img_tensor
+            self.label[i]   = label_tensor
+        
+        # Data Augmentation
+
+        hflip               = transforms.RandomHorizontalFlip(p=1)
+        vflip               = transforms.RandomVerticalFlip(p=1)
+
+        self.data_hflip     = torch.stack([hflip(i) for i in self.data])
+        self.label_hflip    = torch.stack([hflip(i) for i in self.label])
+
+        self.data_vflip     = torch.stack([vflip(i) for i in self.data])
+        self.label_vflip    = torch.stack([vflip(i) for i in self.label])
+
+        self.data_rotation  = torch.stack([self.angle_rotation(i) for i in self.data])
+        self.label_rotation = torch.stack([self.angle_rotation(i) for i in self.label])
+
+        # Concatenate new dataset
+        self.data_augment   = torch.cat((self.data, self.data_hflip, self.data_vflip, self.data_rotation))
+        self.label_augment  = torch.cat((self.label, self.label_hflip, self.label_vflip, self.label_rotation))
+
+
+    def angle_rotation(self, image):
+        '''
+            Args:
+                    image       (Tensor):   input image as tensor
+        '''
+        images = torch.empty(size=(600*len(self.angles), TF.get_image_num_channels(image), 400, 400))
+        count = 0
+        for angle in range(len(self.angles)):
+            img = TF.rotate(image, angle)
+            images[count] = img
+            count+=1
+        return images
 
 
 if __name__ == '__main__':
 
-    feature_extractor = SegformerFeatureExtractor.from_pretrained("nvidia/segformer-b0-finetuned-ade-512-512", size=(400, 400))
-    dataset = SegDataset(path='data/segmentation/raw', feature_extractor=feature_extractor)
+    dataset = SegDataset(path='data/segmentation/raw')
 
     print(dataset[0])
             
